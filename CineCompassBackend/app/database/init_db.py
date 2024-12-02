c5a11c30382f025a34f7b69972769c87344d4c09
bene <benedikt.schmatz@edu.fh-joanneum.at>
2024-12-02 14:53:30 +0100

    Change database init

--- CineCompassBackend/app/database/init_db.py
+++ CineCompassBackend/app/database/init_db.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
@@ -5,8 +6,15 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 Base = declarative_base()
 
 def init_db():
-    directory = Path(__file__).parent
-    db_path = directory / "movies.db"
-    engine = create_engine(f"sqlite:///{db_path}")
+    database_url = os.getenv('DATABASE_URL')
+
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    else:
+        directory = Path(__file__).parent
+        db_path = directory / "movies.db"
+        database_url = f"sqlite:///{db_path}"
+
+    engine = create_engine(database_url)
     Base.metadata.create_all(engine)
     return engine, sessionmaker(bind=engine)
